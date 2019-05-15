ae1d62125230a7c9ee7e5eafc444564a7da87e1c
Agisilaos Kounelis <kounelisagis@gmail.com>
2019-05-15 20:32:44 +0300

    Update bst.c

--- source_codes/bst/bst.c
+++ source_codes/bst/bst.c
@@ -1,260 +1,258 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define N 5
 
 typedef struct student_node student_node;
 
-
-struct student_node{
+struct student_node {
     char name[128];
-    int AM;
+    char AM[128];
     float grade;
-    student_node *left;
-    student_node *right;
+    student_node* left;
+    student_node* right;
 };
 
-
 // returns root
-void insert_node(student_node* root, student_node* new_student) {
+student_node* insert_node(student_node* root, student_node* new_student) {
 
-    if(new_student->AM < root->AM) {
-        if(root->left == NULL)
-            root->left = new_student;
-        else
-            insert_node(root->left, new_student);
-    }
-    else{
-        if(root->right == NULL)
-            root->right = new_student;
-        else
-            insert_node(root->right, new_student);
-    }
+    if (!root)
+        return new_student;
 
+    if (strcmp(new_student->AM, root->AM) < 0)
+        root->left = insert_node(root->left, new_student);
+    else
+        root->right = insert_node(root->right, new_student);
+
+    return root;
 }
-
 
 
 student_node* create_students() {
 
     student_node* root = NULL;
+    FILE* fp;
+    fp = fopen("data.txt", "r");
 
-    int i = 0;
-    for(i=0;i<N;i++) {
-        student_node *new_student;
-        new_student = (student_node*)malloc(sizeof(student_node));
+    while (!feof(fp)) {
 
-        scanf("%s", (new_student->name));
-        scanf("%d", &(new_student->AM));
-        scanf("%f", &(new_student->grade));
+        student_node* new_student;
+        new_student = (student_node*) malloc(sizeof(student_node));
+
+        fscanf(fp, "%s", new_student->AM);
+
+        char temp[128];
+        fscanf(fp, "%s", new_student->name);
+        fscanf(fp, "%s", temp);
+
+        strcat(new_student->name, " ");
+        strcat(new_student->name, temp);
+
+        fscanf(fp, "%f", &(new_student->grade));
         new_student->left = NULL;
         new_student->right = NULL;
 
-        printf("--------\n");
-
-        if(root == NULL)
-            root = new_student;
-        else
-            insert_node(root, new_student);
+        root = insert_node(root, new_student);
     }
-  
-  return root;
+
+    fclose(fp);
+
+    return root;
 
 }
 
-
-/*
-student_node* find(student_node* root, int key){ // returns node with this AM
-  
-  while(root && key != root->AM)
-  {
-    if(key < root->AM)
-      root = root->left;
-    else if(key > root->AM)
-      root = root->right;
-  }
-  
-  return root;
-}
-*/
-
-student_node* find(student_node *root, int key)
-{
-    if(!root || root->AM == key)
+student_node* search(student_node* root, char* key) {
+    if (!root || strcmp(root->AM, key) == 0)
         return root;
-    else if(key > root->AM)
-        return find(root->right, key);
+    else if (strcmp(key, root->AM) > 0)
+        return search(root->right, key);
     else
-        return find(root->left, key);
+        return search(root->left, key);
 }
 
+void modify(student_node* root, char* key) {
 
+    student_node* node = search(root, key);
 
+    if (!node)
+        printf("Not Found!");
+    else {
+        char ans;
+        scanf("%c", &ans);
+        printf("Do you want to change name? | y/n\n");
+        scanf("%c", &ans);
+        if (ans == 'y') {
+            printf("Give new name\n");
+            char temp[128];
+            scanf("%s", node->name);
+            scanf("%s", temp);
 
-void search(student_node* root, int key){
-  
-  student_node* node = find(root, key);
-  
-  if(node == NULL)
-    printf("Not Found!");
-  else {
-    printf("Name:%s\n", node->name);
-    printf("AM:%d\n", node->AM);
-    printf("Grade:%f\n", node->grade);
-  }
-  
-}
-
-void modify(student_node* root, int key){
-  
-  student_node* node = find(root, key);
-  
-  if(!node)
-    printf("Not Found!");
-  else {
-    char ans;
-    scanf("%c", &ans);
-    printf("Do you want to change name? | y/n\n");
-    scanf("%c", &ans);
-    if(ans == 'y'){
-      printf("Give new name\n");
-      scanf("%s", node->name);
+            strcat(node->name, " ");
+            strcat(node->name, temp);
+        }
+        scanf("%c", &ans);
+        printf("Do you want to change grade? | y/n\n");
+        scanf("%c", &ans);
+        if (ans == 'y') {
+            printf("Give new grade\n");
+            scanf("%f", &node->grade);
+        }
     }
-    scanf("%c", &ans);
-    printf("Do you want to change grade? | y/n\n");
-    scanf("%c", &ans);
-    if(ans == 'y'){
-      printf("Give new grade\n");
-      scanf("%f", &node->grade);
-    }
-  }
-  
+
 }
 
+void print_node(student_node* root) {
+	printf("%-30s%-15s%-15f\n", root->name, root->AM, root->grade); 
+}
 
+void in_order_print(student_node* root) {
 
-void in_order_print(student_node* root){
-
-    if(root->left){
-        //printf("l");
+    if (root->left)
         in_order_print(root->left);
-    }
 
-    printf("Name: %s | AM: %d | Grade: %f\n", root->name, root->AM, root->grade);
+    print_node(root);
 
-    if(root->right){
-        //printf("r");
+    if (root->right)
         in_order_print(root->right);
-    }
 }
+
+void print_tree(student_node* root) {
+	printf("%-30s%-15s%-15s\n", "Name", "AM", "Grade");		//print header
+	printf("%.*s\n", 60, "======================================================================================================================");
+	in_order_print(root);
+}
+
 
 // call with root->left
-student_node* rightmost_node(student_node* root){
-    
-	if(!root->right)
-		return root;
-	
-	return rightmost_node(root->right);
+student_node* rightmost_node(student_node* root) {
+
+    if (!root->right)
+        return root;
+
+    return rightmost_node(root->right);
 }
 
+student_node* delete(student_node* root, char* key) {
 
-student_node* delete(student_node* root, int key){
-  
-  if(!root)
-    return NULL;
-  
-  if(key > root->AM)
-    root->right = delete(root->right, key);
-  else if(key < root->AM)
-    root->left = delete(root->left, key);
-  else // key == root->AM
-  {
-    // 0 children
-    if(!root->left && !root->right)
-    {
-      free(root);
-      return NULL;
-    }
-    
-    // 1 childer
-    else if(!root->left || !root->right)
-    {
-      student_node* to_return = root->left ? root->left : root->right;
-      
-      free(root);
-      
-      return to_return;
-    }
-    
-    else  // 2 children
-    {
-      student_node* temp = rightmost_node(root->left);
+    if (!root)
+        return NULL;
 
-      int AM = temp->AM;
-      float grade = temp->grade;
-      //char name[128] = temp->name;
-      
-      delete(root, temp->AM);  // 0 or 1 children	---  ANTI GIA delete(root, AM);
-      
-      root->AM = AM;
-      root->grade = grade;
-      //root->name = name;
-	  
-	  //return root...
+    if (strcmp(key, root->AM) > 0)
+        root->right = delete(root->right, key);
+    else if (strcmp(key, root->AM) < 0)
+        root->left = delete(root->left, key);
+    else // key == root->AM
+    {
+        // 0 children
+        if (!root->left &&!root->right) {
+            free(root);
+            return NULL;
+        }
+
+        // 1 children
+        else if (!root->left || !root->right) {
+            student_node* to_return = root->left ? root->left : root->right;
+
+            free(root);
+
+            return to_return;
+        } else // 2 children
+        {
+            student_node* temp = rightmost_node(root->left);
+
+            char AM[128];
+            strcpy(AM, temp->AM);
+            float grade = temp->grade;
+            char name[128];
+            strcpy(name, temp->name);
+
+            delete(temp, temp->AM); // 0 or 1 children
+
+            strcpy(root->AM, AM);
+            root->grade = grade;
+            strcpy(root->name, name);
+        }
+
     }
-    
-  }
-  
-  return root;
-  
+
+    return root;
+
 }
 
+void create_custom_node(student_node* root) {
 
+    student_node* new_student;
+    new_student = (student_node* ) malloc(sizeof(student_node));
 
-// 6 -> 7 -> 10-|> 10 -> 65
+    scanf("%s", new_student->AM);
 
+    char temp[128];
+    scanf("%s", new_student->name);
+    scanf("%s", temp);
 
+    strcat(new_student->name, " ");
+    strcat(new_student->name, temp);
 
+    scanf("%f", &(new_student->grade));
+    new_student->left = NULL;
+    new_student->right = NULL;
 
+    root = insert_node(root, new_student);
 
-/*
-                   20
-                 /    \
-                /      \
-               5       30
-             /   \     /\
-            /     \   /  \
-           1      15 25  40
-                /     \    \
-               /       \    \
-              9        29   45
-            /   \          /
-           /     \        /
-          7      12      42
-*/
+}
 
-
-
-
-int main()
-{
+int main() {
     student_node* root;
     root = create_students();
-  
-    in_order_print(root);
-  
-    int key;
-    //search(root, key);
-  
-    printf("\n---\nInsert key to modify: ");
-    scanf("%d", &key);
-    modify(root, key);
-  
-    printf("\n---\nInsert key to delete: ");
-    scanf("%d", &key);
-    delete(root, key);
-  
-    in_order_print(root);
+
+    int choice = -1;
+    char key[128];
+
+    while (choice != 5) {
+        printf("Options:\n0.Search\n1.Insert\n2.Delete\n3.Modify\n4.Print Tree\n5.Exit\n");
+        scanf("%u", &choice);
+
+        switch (choice) {
+        case 0:
+            // search
+            printf("\n---\nInsert key to search: ");
+            scanf("%s", key);
+            student_node* node = search(root, key);
+            if (node == NULL)
+                printf("Not Found!");
+            else {
+                printf("Name:%s\n", node->name);
+                printf("AM:%s\n", node->AM);
+                printf("Grade:%f\n", node->grade);
+            }
+            break;
+        case 1:
+            create_custom_node(root);
+            break;
+        case 2:
+            // delete
+            printf("\n---\nInsert key to delete: ");
+            scanf("%s", key);
+            delete(root, key);
+            break;
+        case 3:
+            // modify
+            printf("\n---\nInsert key to modify: ");
+            scanf("%s", key);
+            modify(root, key);
+            break;
+        case 4:
+            // print tree
+            print_tree(root);
+            break;
+        case 5:
+            // exit
+            break;
+        default:
+            printf("Invalid Option!\n");
+        }
+
+    }
 
     return 0;
 }
